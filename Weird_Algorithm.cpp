150554a75be8f866965b643bff54f4d7b37ceabf
sachin108 <88074105+sachin108@users.noreply.github.com>
2021-09-07 00:45:45 +0530

    Create Weird_Algorithm.cpp

---
+++ Weird_Algorithm.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+	ios::sync_with_stdio(0);
+	cin.tie(0);	
+	long long n;
+	cin>>n;
+	while(true){
+		cout<<n<<"	";
+		if(n==1)
+			break;
+		if(n%2==0){
+			n=n/2;
+		}	
+		else{
+			n=n*3;
+			++n;
+		}
+	}
+}
