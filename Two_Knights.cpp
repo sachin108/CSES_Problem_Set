4b49021035d0964c25c93a6c56eeb51658cd351a
Sachin rajput <sachin108rajput@hotmail.com>
2022-11-21 21:24:37 +0530

    21st Nov

--- Two_Knights.cpp
+++ Two_Knights.cpp
@@ -2,7 +2,6 @@
 
 #include <bits/stdc++.h>
 using namespace std;
-
 int main() {
     long long n;
     cin >> n;
@@ -11,6 +10,5 @@ int main() {
     for (long long i = 2 ; i <= n ; i++) {
       cout<<(((i*i) * (i*i-1)) / 2) - ((4*(i-2) * (i-1))) << endl;
     }
-
     return 0;
 }
